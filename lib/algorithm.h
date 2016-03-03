d54350ec8681acbc170308490235847f32359a9c
cxsmarkchan <cxsmarkchan@163.com>
2016-03-03 21:14:06 +0800

    swap函数

---
+++ lib/algorithm.h
@@ -0,0 +1,19 @@
+#pragma once
+//algorithm.h
+//一些相关的算法和通用操作
+
+#ifndef _CXS_ALGORITHM_H_
+#define _CXS_ALGORITHM_H_
+
+#include <iostream>
+#include "cxs-commons.h"
+
+//swap
+template<class T>
+void swap(T& var1, T& var2){
+	T tmp = std::move(var1);
+	var1 = std::move(var2);
+	var2 = std::move(tmp);
+}
+
+#endif
---
+++ test/TestAlgorithm.cpp
@@ -0,0 +1,70 @@
+#include "stdafx.h"
+#include "algorithm.h"
+
+using namespace System;
+using namespace System::Text;
+using namespace System::Collections::Generic;
+using namespace Microsoft::VisualStudio::TestTools::UnitTesting;
+using namespace cxs;
+
+namespace test
+{
+	[TestClass]
+	public ref class TestAlgorithm
+	{
+	private:
+		TestContext^ testContextInstance;
+
+	public: 
+		/// <summary>
+		///获取或设置测试上下文，该上下文提供
+		///有关当前测试运行及其功能的信息。
+		///</summary>
+		property Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^ TestContext
+		{
+			Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^ get()
+			{
+				return testContextInstance;
+			}
+			System::Void set(Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^ value)
+			{
+				testContextInstance = value;
+			}
+		};
+
+		#pragma region Additional test attributes
+		//
+		//编写测试时，可以使用以下附加特性:
+		//
+		//在运行类中的第一个测试之前，使用 ClassInitialize 来运行代码
+		//[ClassInitialize()]
+		//static void MyClassInitialize(TestContext^ testContext) {};
+		//
+		//在类中的所有测试都已运行之后，使用 ClassCleanup 来运行代码
+		//[ClassCleanup()]
+		//static void MyClassCleanup() {};
+		//
+		//在运行每个测试之前，使用 TestInitialize 来运行代码
+		//[TestInitialize()]
+		//void MyTestInitialize() {};
+		//
+		//在每个测试运行完之后，使用 TestCleanup 来运行代码
+		//[TestCleanup()]
+		//void MyTestCleanup() {};
+		//
+		#pragma endregion 
+
+		[TestMethod]
+		void TestSwap()
+		{
+			//
+			// TODO: 在此处添加测试逻辑
+			//
+			int a = 1, b = 2;
+			swap(a, b);
+			Assert::AreEqual(a, 2);
+			Assert::AreEqual(b, 1);
+			
+		};
+	};
+}
