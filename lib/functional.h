3697f7115c3d69e2f1e3151577b6700a073d2b85
cxsmarkchan <cxsmarkchan@163.com>
2016-03-08 00:02:03 +0800

    添加了equal和equal_to模板类

--- lib/functional.h
+++ lib/functional.h
@@ -23,6 +23,23 @@ struct greater{
 	}
 };
 
+template<class T>
+struct equal{
+	bool operator()(const T& first, const T& second) const{
+		return first == second;
+	}
+};
+
+template<class T>
+struct equal_to{
+	const T _benchmark;
+	equal_to(const T& benchmark):_benchmark(benchmark){}
+	bool operator()(const T& elem) const{
+		return elem == _benchmark;
+	}
+};
+
+
 _CXS_NS_END
 
 #endif
